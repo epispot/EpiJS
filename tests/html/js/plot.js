99b14d5ee3166cd70fd097409da8e87cc52ec33e
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-03-07 18:36:47 -0800

    Update EpiJS/plot.js and web/plot.js - Add seir model - Add seird model - Change web/plot.min.js to web/plot.js - Add seir and seird models to tests.

--- tests/html/js/plot.js
+++ tests/html/js/plot.js
@@ -1,8 +1,8 @@
 let rdisplay = document.getElementById('rdisplay')
-let sirchart = document.getElementById('ctx')
+let sirchart = document.getElementById('ctxsir')
+let seirchart = document.getElementById('ctxseir')
+let seirdchart = document.getElementById('ctxseird')
 
-let rval = r(0.0028717, 8280000, 202.08)
-
-sir(sirchart, 1, 9999, 1, 100, 0.1, 10000)
-
-rdisplay.innerHTML += rval
+sir(sirchart, 4, 9999, 1, 100, 1/21, 10000)
+seir(seirchart, 4, 9999, 1, 100, 1/7, 1/7, 10000)
+seird(seirdchart, 4, 99999, 1, 100, 1/21, 1/14, 1/100, 10000)
