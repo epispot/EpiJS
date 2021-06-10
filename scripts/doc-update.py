c44abed3b4af3810cc7075e218fdc9df5aa10a9c
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-06-10 13:41:23 -0700

    Fix documentation bug

--- scripts/doc-update.py
+++ scripts/doc-update.py
@@ -1,9 +1,8 @@
-"""
 with open('../docs/pre.md', 'r') as predoc:
   doc = predoc.readlines()
-  doc[18] = '# Pre\n'
-  doc[4] = ''
+  doc[22] = '# Pre\n'
+  doc[5] = '\n'
+  doc[4] = '\n'
 
 with open('../docs/pre.md', 'w') as predoc:
   predoc.writelines(doc)
-"""
