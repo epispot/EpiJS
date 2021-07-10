4282948cd970947c6ae11f7b5e8c57802aa8aa57
QLabs <55121845+Quantalabs@users.noreply.github.com>
2021-07-09 17:31:51 -0700

    Add browserify

---
+++ web/funcs.js
@@ -0,0 +1,11 @@
+
+let sir = EpiJS.pre.sir
+let seir = EpiJS.pre.seir
+let seird = EpiJS.pre.seird
+let seihrd = EpiJS.pre.seihrd
+let seihcrd = EpiJS.pre.seihcrd
+let Virus = EpiJS.com.Virus 
+let Community = EpiJS.com.Community
+let compare = EpiJS.com.compare
+let Idiom = EpiJS.comp.Idiom
+let Model = EpiJS.model.Model
