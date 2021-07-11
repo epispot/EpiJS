epispot/EpiJS

commit 4900cae1c50b89618b2dcac71525fbe2f7cf6e94

Add code for tests
Add code for tests, which include all modules and all functions in modules.

CHANGELIST:
  * Add tests for com module
  * Add tests for comp module
  * Add tests for model module
  * Add tests for pre module
  * Update HTML for testing
  * Fix error handling on test
  * Change Community class name from Com to Community in funcs.js and index.js.
  * Remove "index.js" test file.

Signed-off-by: Quantalabs <55121845+Quantalabs@users.noreply.github.com>

web/funcs.js
@@ -5,7 +5,7 @@ let seird = EpiJS.pre.seird
 let seihrd = EpiJS.pre.seihrd
 let seihcrd = EpiJS.pre.seihcrd
 let Virus = EpiJS.com.Virus 
-let Com = EpiJS.com.Community
+let Community = EpiJS.com.Community
 let compare = EpiJS.com.compare
 let Idiom = EpiJS.comp.Idiom
 let Model = EpiJS.model.Model

web/index.js
@@ -103653,7 +103653,7 @@ let seird = EpiJS.pre.seird
 let seihrd = EpiJS.pre.seihrd
 let seihcrd = EpiJS.pre.seihcrd
 let Virus = EpiJS.com.Virus 
-let Com = EpiJS.com.Community
+let Community = EpiJS.com.Community
 let compare = EpiJS.com.compare
 let Idiom = EpiJS.comp.Idiom
 let Model = EpiJS.model.Model

tests/index.js
@@ -17,7 +17,7 @@ const puppeteer = require('puppeteer');
   });
 
   if (errors > 1) {
-    Promise.reject('More than one error occurred. See log for details.');
+    Promise.reject(new Error('More than one error occurred. See log for details.'));
   }
 
   await browser.close();

tests/scripts/com.js
@@ -1,0 +1,26 @@
+let community = new Community(10000, 1, 9999)
+let covid = new Virus(4, 1/21, 1/14, 1/25)
+let anothervirus = new Virus(4, 1/10, 1/12, 1/50)
+
+let sirout = community.sir(covid, 100)
+let sirout2 = community.sir(anothervirus, 100)
+
+let seirout = community.seir(covid, 100)
+let seirout2 = community.seir(anothervirus, 100)
+
+let seirdout = community.seird(covid, 200)
+let seirdout2 = community.seird(anothervirus, 200)
+
+compare("canvas-com1", sirout, sirout2, "1", "2", 100)
+compare("canvas-com2", seirout, seirout2, "1", "2", 100)
+compare("canvas-com3", seirdout, seirdout2, "1", "2", 200)
+
+NewYorkCity = new Community(8419000, 300, 8418700)
+covid = new Virus(5.7, 2.1/100)
+covid2 = new Virus(6, 2.1/100)
+
+let newoutbreak = NewYorkCity.custom([[susceptible, "S"], [infected, "I"], [recovered, "R"]], 200, covid, {B: covid.rnaught*covid.u})
+let newoutbreak2 = NewYorkCity.custom([[susceptible, "S"], [infected, "I"], [recovered, "R"]], 200, covid2, {B: covid2.rnaught*covid.u})
+compare("canvas-com4", newoutbreak, newoutbreak2, "1", "2", 200)
+console.log(newoutbreak)
+console.log(newoutbreak2)

tests/scripts/comp.js
@@ -1,0 +1,3 @@
+let susceptible = new Idiom("S-(B*S*I/p)");
+let infected = new Idiom("I+(B*S*I/p)-(u*I)");
+let recovered = new Idiom("R+(u*I)");

tests/scripts/model.js
@@ -1,0 +1,12 @@
+let key = {
+    "S": 10000,
+    "B": 0.3,
+    "I": 100,
+    "R": 0,
+    "p": 10100,
+    "u": 0.2
+};
+
+let sirm = new Model([[susceptible, "S"], [infected, "I"], [recovered, "R"]], key)
+var data = sirm.get_data(100)
+document.getElementById('model').innerHTML = 'S: '+data['S']+'<br>'+'I: '+data['I']+'<br>'+'R: '+data['R']

tests/scripts/pre.js
@@ -1,0 +1,5 @@
+sir('canvas-pre1', 4, 9999, 1, 100, 1/21, 10000)
+seir('canvas-pre2', 4, 9999, 1, 100, 1/7, 1/7, 10000)
+seird('canvas-pre3', 4, 9999, 1, 265, 1/21, 1/14, 1/100, 10000)
+seihrd('canvas-pre4', 4, 9999, 1, 265, 1/21, 1/40, 1/14, 1/100, 1/20, 1/30, 10000)
+seihcrd('canvas-pre5', 4, 9999, 1, 265, 1/21, 1/40, 1/14, 1/100, 1/20, 1/10, 1/40, 2/5, 1/5, 1/5, 1/5, 1/30, 10000)

tests/test.html
@@ -7,23 +7,29 @@
     <title>EpiJS Test</title>
 </head>
 <body>
-    <canvas id='pre'>
+    <div id='pre'>
+        <canvas id='canvas-pre1'></canvas>
+        <canvas id='canvas-pre2'></canvas>
+        <canvas id='canvas-pre3'></canvas>
+        <canvas id='canvas-pre4'></canvas>
+        <canvas id='canvas-pre5'></canvas>
+    </div>
+    <div id='comp'>
 
-    </canvas>
-    <canvas id='comp'>
+    </div>
+    <div id='com'>
+        <canvas id='canvas-com1'></canvas>
+        <canvas id='canvas-com2'></canvas>
+        <canvas id='canvas-com3'></canvas>
+        <canvas id='canvas-com4'></canvas>
+    </div>
+    <div id="model">
 
-    </canvas>
-    <canvas id='com'>
-
-    </canvas>
-    <canvas id="model">
-
-    </canvas>
+    </div>
     <script src="../web/index.js"></script>
     <script src="scripts/pre.js"></script>
     <script src="scripts/comp.js"></script>
-    <script src="scripts/com.js"></script>
     <script src="scripts/model.js"></script>
-    <script src="scripts/index.js"></script>
+    <script src="scripts/com.js"></script>
 </body>
 </html>

commit cb728a85a700c7750548914aa9da2175a26f74ac

[#46] CI/CD (#47)
* Add README changes from https://readme.so

* [#46] Complete basic CI setup

Complete basic CI setup, with travis ci and tests using puppeteer. Puppeteer opens the test html file and throws an error if more than one error occurs.

CHANGELIST:

  * Add test script with puppeteer
  * Add travis ci
  * Add new dependencies to package.json and package-lock.json
  * Remove exisiting test files
  * Create new html test file which has external javascript files linked for each module

Signed-off-by: Quantalabs <55121845+Quantalabs@users.noreply.github.com>

* Remove travis CI and transfer to github actions

Due to faster times and unlimited workflow minutes, we are instead using github actions, and not travis ci.

CHANGELIST:
  * Remove travis workflow
  * Update dependabot for extra capabilities
  * Update PR Labeler
  * Transfer travis workflow to build workflow
  * Remove unnecassary workflows

Signed-off-by: Quantalabs <55121845+Quantalabs@users.noreply.github.com>

* Expand build workflow

Expand build workflow to run on push to any path, and change tests and build ouptut to web/index.js instead of web/index.min.js, as the code is not minfied.

* Update dependencies

Add browserify to dev deps.

CHANGELIST:
  * Add broserify to dev dependencies
  * Run npm audit fix to fix errors on build workflow

Signed-off-by: Quantalabs <55121845+Quantalabs@users.noreply.github.com>

* Continue on error when installing dependencies

Continues on error when instaling dependencies.

Signed-off-by: Quantalabs <55121845+Quantalabs@users.noreply.github.com>

* Add extra deepsource configuration

Add extra deepsource configuration including test patterns and exclude patterns.

CHANGELIST:
  * Add test patterns to deepsource configuration
  * Add exclude patterns to deepsource configuration to exclude files
  * Add build workflow badge to README

Signed-off-by: Quantalabs <55121845+Quantalabs@users.noreply.github.com>

* Add code for tests

Add code for tests, which include all modules and all functions in modules.

CHANGELIST:
  * Add tests for com module
  * Add tests for comp module
  * Add tests for model module
  * Add tests for pre module
  * Update HTML for testing
  * Fix error handling on test
  * Change Community class name from Com to Community in funcs.js and index.js.
  * Remove "index.js" test file.

Signed-off-by: Quantalabs <55121845+Quantalabs@users.noreply.github.com>

* Fix deepsource errors introduced in 4900cae1c50b89618b2dcac71525fbe2f7cf6e94

.deepsource.toml
@@ -1,5 +1,9 @@
 version = 1
 
+test_patterns = ["tests/**"]
+
+exclude_patterns = ["web/**"]
+
 [[analyzers]]
 name = "javascript"
 enabled = true

.github/dependabot.yml
@@ -4,3 +4,11 @@ updates:
     directory: "/"
     schedule:
       interval: "daily"
+    assignees:
+      - "Quantalabs"
+    commit-message:
+      prefix: "[DEP]"
+      prefix-development: "[DEV/DEP]"
+      include: "scope"
+    labels:
+      - "dependencies"

.github/labeler.yml
@@ -2,21 +2,10 @@ tests:
   - tests/*
 
 src:
-  - epijs/*
-  - bin/*
+  - EpiJS/*
   - package.json
   - package-lock.json
-  - README.md
+  - web/*
 
-repo:
+CI/CD:
   - .github/*
-
-workflows:
-  - .github/workflows
-  - .github/labeler.yml
-
-issue templates:
-  - .github/ISSUE_TEMPLATES/*
-
-pull request:
-  - .github/PULL_REQUEST.md

.github/workflows/build.yml
@@ -1,30 +1,27 @@
-name: Node.js CI
+name: Build
 
-on:
-  push:
-    paths:
-      - EpiJS/
+on: [push, pull_request]
 
 jobs:
   build:
     runs-on: ubuntu-latest
     strategy:
       matrix:
-        node-version: [10.x, 12.x, 14.x, 15.x]
+        node-version: [10.x, 12.x, 14.x, 15.x, 16.x]
     steps:
     - uses: actions/checkout@v2
     - name: Use Node.js ${{ matrix.node-version }}
       uses: actions/setup-node@v1
       with:
         node-version: ${{ matrix.node-version }}
-    - run: npm ci
-    - run: npm run build
-    - name: Configure Git
+    - name: Install Dependencies
+      continue-on-error: true  
+      run:  |
+        npm install
+        npm audit fix
+    - name: Run Browserify
       run: |
-        git config user.name github-actions
-        git config user.email github-actions@github.com
-    - name: Push changes (if necassary)
+        npm run build
+    - name: Run Tests
       run: |
-        git add .
-        git commit -m "Update minified JS"
-        git push --set-upstream origin main
+        npm run test

.github/workflows/dep_merge.yml
@@ -1,31 +1,0 @@
-name: Merge me!
-
-on:
-  workflow_run:
-    types:
-      - completed
-    workflows:
-      - 'CodeQL / Analyze (javascript) (pull_request)'
-
-jobs:
-  merge-me:
-    name: Merge me!
-
-    runs-on: ubuntu-latest
-
-    steps:
-      - name: Merge me!
-        if: ${{ github.event.workflow_run.conclusion == 'success' }}
-        uses: ridedott/merge-me-action@v2
-        with:
-          # Depending on branch prodtection rules, a  manually populated
-          # `GITHUB_TOKEN_WORKAROUND` secret with permissions to push to
-          # a protected branch must be used.
-          #
-          # When using a custom token, it is recommended to leave the following
-          # comment for other developers to be aware of the reasoning behind it:
-          #
-          # This must be used as GitHub Actions token does not support pushing
-          # to protected branches.
-          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-          PRESET: DEPENDABOT_MINOR

.github/workflows/nightly.yml
@@ -1,51 +1,0 @@
-name: Nightly NPM Publish
-
-on:
-  push:
-    branches: 
-      - nightly
-    paths:
-      - package.json
-      - JSLY/*
-      - README.md
-
-jobs:
-    build:
-        runs-on: ubuntu-latest
-        steps:
-            - uses: actions/checkout@v2
-            - uses: actions/setup-node@v1
-              with:
-                  node-version: 12
-            - run: npm ci
-            - run: npm test
-
-    publish-npm:
-        needs: build
-        runs-on: ubuntu-latest
-        steps:
-            - uses: actions/checkout@v2
-            - uses: actions/setup-node@v1
-              with:
-                  node-version: 12
-                  registry-url: "https://registry.npmjs.org"
-            - run: npm install
-            - run: npm publish
-              env:
-                  NODE_AUTH_TOKEN: ${{ secrets.NPM_TOKEN }}
-
-    publish-gpr:
-        needs: build
-        runs-on: ubuntu-latest
-        steps:
-            - uses: actions/checkout@v2
-            - uses: actions/setup-node@v1
-              with:
-                  node-version: 12
-                  registry-url: "https://npm.pkg.github.com"
-                  scope: "@quantalabs"
-            - run: npm install
-            - run: echo "registry=https://npm.pkg.github.com/@quantalabs" >> .npmrc
-            - run: npm publish
-              env:
-                  NODE_AUTH_TOKEN: ${{ secrets.GITHUB_TOKEN }}

.github/workflows/npm-publish.yml
@@ -4,8 +4,6 @@
 name: Publish NPM only
 
 on:
-  release:
-    types: [created]
   workflow_dispatch:
 
 jobs:

web/funcs.js
@@ -5,7 +5,7 @@ let seird = EpiJS.pre.seird
 let seihrd = EpiJS.pre.seihrd
 let seihcrd = EpiJS.pre.seihcrd
 let Virus = EpiJS.com.Virus 
-let Com = EpiJS.com.Community
+let Community = EpiJS.com.Community
 let compare = EpiJS.com.compare
 let Idiom = EpiJS.comp.Idiom
 let Model = EpiJS.model.Model

web/index.js
@@ -103610,10 +103610,10 @@ module.exports={
     "description": "A disease modeling package.",
     "main": "EpiJS/index.js",
     "scripts": {
-        "test": "echo \"Error: no test specified\" && exit 1",
+        "test": "node tests/index.js",
         "docs:dev": "bash scripts/doc.sh",
         "docs:build": "bash scripts/doc-build.sh",
-        "build": "browserify EpiJS --s EpiJS -o web/index.min.js"
+        "build": "browserify EpiJS --s EpiJS -o web/index.js && cd web && cat funcs.js >> index.js"
     },
     "repository": {
         "type": "git",
@@ -103633,6 +103633,7 @@ module.exports={
     "devDependencies": {
         "documentation": "^13.2.5",
         "mocha": "^9.0.0",
+        "puppeteer": "^10.1.0",
         "vuepress": "^1.8.2",
         "webpack": "^5.40.0",
         "webpack-cli": "^4.7.2"
@@ -103645,13 +103646,14 @@ module.exports={
 
 },{}]},{},[6])(6)
 });
+
 let sir = EpiJS.pre.sir
 let seir = EpiJS.pre.seir
 let seird = EpiJS.pre.seird
 let seihrd = EpiJS.pre.seihrd
 let seihcrd = EpiJS.pre.seihcrd
 let Virus = EpiJS.com.Virus 
-let Com = EpiJS.com.Community
+let Community = EpiJS.com.Community
 let compare = EpiJS.com.compare
 let Idiom = EpiJS.comp.Idiom
 let Model = EpiJS.model.Model

tests/index.html
@@ -1,53 +1,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EpiJS Test</title>
-</head>
-<body>
-    <div id="rdisplay">
-
-    </div>
-    <canvas id="ctxsir"></canvas>
-    <canvas id="ctxseir"></canvas>
-    <canvas id="ctxseird"></canvas>
-    <canvas id="ctxseihrd"></canvas>
-    <canvas id="ctxseihcrd"></canvas>
-    <canvas id="c1"></canvas>
-    <canvas id="c2"></canvas>
-    <canvas id="sir_compare"></canvas>
-    <canvas id="seir_compare"></canvas>
-    <canvas id="seird_compare"></canvas>
-    <canvas id="new_compare"></canvas>
-    <script src="https://cdn.jsdelivr.net/npm/mathjs@9.4.2/lib/browser/math.min.js"></script>
-    <script src="../web/com.min.js"></script>
-    <script src="../web/pre.min.js"></script>
-    <script src="../web/comp.min.js"></script>
-    <script src="../web/model.min.js"></script>
-    <script src="js/chart.min.js"></script>
-    <script src="js/pre.js"></script>
-
-    <!-- Testing browserify code -->
-    <script src="../web/index.min.js"></script>
-    <script>
-        susceptible = new Idiom("S-(B*S*I/p)");
-        infected = new Idiom("I+(B*S*I/p)-(u*I)");
-        recovered = new Idiom("R+(u*I)")
-
-        key = {
-            "S": 10000,
-            "B": 0.3,
-            "I": 100,
-            "R": 0,
-            "p": 10100,
-            "u": 0.2
-        };
-
-        sirm = new Model([[susceptible, "S"], [infected, "I"], [recovered, "R"]], key)
-        data = sirm.get_data(100)
-        console.log(data);
-    </script>
-</body>
-</html>

tests/index.js
@@ -1,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`file://${__dirname}/test.html`);
+  
+  let errors = 0
+  
+  page.on('error', err=> {
+    console.log('error happen at the page: ', err);
+    errors += 1
+  });
+  page.on('pageerror', pageerr=> {
+    console.log('pageerror occurred: ', pageerr);
+    errors += 1
+  });
+
+  if (errors > 1) {
+    Promise.reject(new Error('More than one error occurred. See log for details.'));
+  }
+
+  await browser.close();
+})();
+

tests/js/pre.js
@@ -1,57 +1,0 @@
-let rdisplay = document.getElementById('rdisplay')
-let sirchart = document.getElementById('ctxsir')
-let seirchart = document.getElementById('ctxseir')
-let seirdchart = document.getElementById('ctxseird')
-let seihrdmodelchart = document.getElementById('ctxseihrd')
-let seihcrdchart = document.getElementById('ctxseihcrd')
-
-sir(sirchart, 4, 9999, 1, 100, 1/21, 10000)
-seir(seirchart, 4, 9999, 1, 100, 1/7, 1/7, 10000)
-seird(seirdchart, 4, 9999, 1, 265, 1/21, 1/14, 1/100, 10000)
-seihrd(seihrdmodelchart, 4, 9999, 1, 265, 1/21, 1/40, 1/14, 1/100, 1/20, 1/30, 10000)
-seihcrd(seihcrdchart, 4, 9999, 1, 265, 1/21, 1/40, 1/14, 1/100, 1/20, 1/10, 1/40, 2/5, 1/5, 1/5, 1/5, 1/30, 10000)
-
-let community = new Community(10000, 1, 9999)
-let covid = new Virus(4, 1/21, 1/14, 1/25)
-let anothervirus = new Virus(4, 1/10, 1/12, 1/50)
-
-let sirout = community.sir(covid, 100)
-let sirout2 = community.sir(anothervirus, 100)
-
-let seirout = community.seir(covid, 100)
-let seirout2 = community.seir(anothervirus, 100)
-
-let seirdout = community.seird(covid, 200)
-let seirdout2 = community.seird(anothervirus, 200)
-
-compare("sir_compare", sirout, sirout2, "1", "2", 100)
-compare("seir_compare", seirout, seirout2, "1", "2", 100)
-compare("seird_compare", seirdout, seirdout2, "1", "2", 200)
-
-
-let susceptible = new Idiom("S-(B*S*I/p)");
-let infected = new Idiom("I+(B*S*I/p)-(u*I)");
-let recovered = new Idiom("R+(u*I)");
-
-let key = {
-  "S": 10000,
-  "B": 0.3,
-  "I": 100,
-  "R": 0,
-  "p": 10100,
-  "u": 0.2
-};
-
-let sirm = new Model([[susceptible, "S"], [infected, "I"], [recovered, "R"]], key)
-var data = sirm.get_data(100)
-console.log(data);
-
-NewYorkCity = new Community(8419000, 300, 8418700)
-covid = new Virus(5.7, 2.1/100)
-covid2 = new Virus(6, 2.1/100)
-
-let newoutbreak = NewYorkCity.custom([[susceptible, "S"], [infected, "I"], [recovered, "R"]], 200, covid, {B: covid.rnaught*covid.u})
-let newoutbreak2 = NewYorkCity.custom([[susceptible, "S"], [infected, "I"], [recovered, "R"]], 200, covid2, {B: covid2.rnaught*covid.u})
-compare("new_compare", newoutbreak, newoutbreak2, "1", "2", 200)
-console.log(newoutbreak)
-console.log(newoutbreak2)

tests/scripts/com.js
@@ -1,0 +1,26 @@
+let community = new Community(10000, 1, 9999)
+let covid = new Virus(4, 1/21, 1/14, 1/25)
+let anothervirus = new Virus(4, 1/10, 1/12, 1/50)
+
+let sirout = community.sir(covid, 100)
+let sirout2 = community.sir(anothervirus, 100)
+
+let seirout = community.seir(covid, 100)
+let seirout2 = community.seir(anothervirus, 100)
+
+let seirdout = community.seird(covid, 200)
+let seirdout2 = community.seird(anothervirus, 200)
+
+compare("canvas-com1", sirout, sirout2, "1", "2", 100)
+compare("canvas-com2", seirout, seirout2, "1", "2", 100)
+compare("canvas-com3", seirdout, seirdout2, "1", "2", 200)
+
+NewYorkCity = new Community(8419000, 300, 8418700)
+covid = new Virus(5.7, 2.1/100)
+covid2 = new Virus(6, 2.1/100)
+
+let newoutbreak = NewYorkCity.custom([[susceptible, "S"], [infected, "I"], [recovered, "R"]], 200, covid, {B: covid.rnaught*covid.u})
+let newoutbreak2 = NewYorkCity.custom([[susceptible, "S"], [infected, "I"], [recovered, "R"]], 200, covid2, {B: covid2.rnaught*covid.u})
+compare("canvas-com4", newoutbreak, newoutbreak2, "1", "2", 200)
+console.log(newoutbreak)
+console.log(newoutbreak2)

tests/scripts/comp.js
@@ -1,0 +1,3 @@
+let susceptible = new Idiom("S-(B*S*I/p)");
+let infected = new Idiom("I+(B*S*I/p)-(u*I)");
+let recovered = new Idiom("R+(u*I)");

tests/scripts/pre.js
@@ -1,0 +1,5 @@
+sir('canvas-pre1', 4, 9999, 1, 100, 1/21, 10000)
+seir('canvas-pre2', 4, 9999, 1, 100, 1/7, 1/7, 10000)
+seird('canvas-pre3', 4, 9999, 1, 265, 1/21, 1/14, 1/100, 10000)
+seihrd('canvas-pre4', 4, 9999, 1, 265, 1/21, 1/40, 1/14, 1/100, 1/20, 1/30, 10000)
+seihcrd('canvas-pre5', 4, 9999, 1, 265, 1/21, 1/40, 1/14, 1/100, 1/20, 1/10, 1/40, 2/5, 1/5, 1/5, 1/5, 1/30, 10000)

tests/test.html
@@ -1,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EpiJS Test</title>
+</head>
+<body>
+    <div id='pre'>
+        <canvas id='canvas-pre1'></canvas>
+        <canvas id='canvas-pre2'></canvas>
+        <canvas id='canvas-pre3'></canvas>
+        <canvas id='canvas-pre4'></canvas>
+        <canvas id='canvas-pre5'></canvas>
+    </div>
+    <div id='comp'>
+
+    </div>
+    <div id='com'>
+        <canvas id='canvas-com1'></canvas>
+        <canvas id='canvas-com2'></canvas>
+        <canvas id='canvas-com3'></canvas>
+        <canvas id='canvas-com4'></canvas>
+    </div>
+    <div id="model">
+
+    </div>
+    <script src="../web/index.js"></script>
+    <script src="scripts/pre.js"></script>
+    <script src="scripts/comp.js"></script>
+    <script src="scripts/model.js"></script>
+    <script src="scripts/com.js"></script>
+</body>
+</html>