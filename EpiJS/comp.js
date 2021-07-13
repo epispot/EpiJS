epispot/EpiJS

commit c79c74e35cd13d751b98a59aeff0c7474ac0cb84

[#37] Add support for stochastic models in the `comp` module.
Add support for stochastic models in the `comp` module, which is defined in initialization. This allows for more flexibility in EpiJS, instead of manually passing in noise into the function. This also works in the Idiom class, however, EpiJS does not auto generate the equations, but will generate noise with the Gaussian distribution.

CHANGELIST:
  * Add tests for the new stochastic models in comp
  * Add stochastic model support in comp
  * Update build with latest functions

Signed-off-by: @Quantalabs <55121845+Quantalabs@users.noreply.github.com>

EpiJS/comp.js
@@ -8,6 +8,7 @@
  */
 
 const math = require('mathjs')
+const gaussian = require('gaussian')
 
 /**
  * Class for a custom compartments.
@@ -21,6 +22,7 @@ const math = require('mathjs')
  *         - 'C' - Critical
  *         - 'H' - Hospitalized
  *         - 'V' - Vaccinated
+ *         - 'w' - Reserved for stochastic models. If put, it will be replaced with a random number generated from the gaussian distribution.
  * @example
  *
  *      let susceptible = new Idiom("S-(B*S*I)")
@@ -47,6 +49,10 @@ class Idiom {
       if (parsed[y] in key) {
         parsed.splice(y, 1, key[parsed[y]])
       }
+      else if (parsed[y] === "w") {
+        distribution = new gaussian(0, 1)
+        parsed.splice(y, 1, distribution.random(1)[0])
+      }
     }
 
     return math.evaluate(parsed.join(''))
@@ -60,23 +66,33 @@ class Idiom {
  *       with the compartment id (one letter only), as a string, and the rate for the compartment.
  *       If reffering to this compartment's population, use "S" as the id. This parameter is useful
  *       if you want to model a disease with re-susceptibility.
+ * @param {Boolean} stochastic If true, the compartment will be stochastic. You can still pass in your normal equation, and epijs will
+ *       auto generate the equations from what you pass in. Defaults to false, optional.
  * @example
  * 
  *      // Note that you can pass in a number as a rate too, 
  *      // but we use a string because we want to multiply 
  *      // by other compartment populations. This applies to the prev parameter too. 
- *      let S = new Susceptible(["I*0.4/N"], []) 
+ *      let S = new Susceptible(["I*0.4/N"], [], true) 
 */
 class Susceptible {
-  constructor (next, prev) {
+  constructor (next, prev, stochastic=false) {
     this.equation = "S"
     
     for (var x in next) {
-      this.equation += "-(S*" + String(next[x]) + ")"
+      if (stochastic == true) {
+        this.equation += "-((S*" + String(next[x]) + ")+sqrt(S*"+String(next[x])+"))*w"
+      } else {
+        this.equation += "-(S*" + String(next[x]) + ")"
+      }
     }
 
     for (var y in prev) {
-      this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      if (stochastic == true) {
+        this.equation += "+(("+String(prev[y][0])+"*" + String(prev[y][1])+")+sqrt("+String(prev[y][0])+"*" + String(prev[y][1])+"))*w"
+      } else {
+        this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      }
     }
   }
 
@@ -95,6 +111,10 @@ class Susceptible {
     for (y in parsed) {
       if (parsed[y] in key) {
         parsed.splice(y, 1, key[parsed[y]])
+      }
+      else if (parsed[y] === "w") {
+        distribution = new gaussian(0, 1)
+        parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
 
@@ -108,6 +128,7 @@ class Susceptible {
  * @param {Array} prev List of rates of the previous compartments, which include sub-arrays
  *       with the compartment id (one letter only), as a string, and the rate for the compartment.
  *       If reffering to this compartment's population, use "I" as the id.
+ * @param {Boolean} stochastic If true, the compartment will be stochastic. Defaults to false, optional.
  * @example
  * 
  *      // Note that you can pass in a string as a rate too, 
@@ -116,15 +137,23 @@ class Susceptible {
  *      let I = new Infected([0.3], [["S", "I*0.4/N"]]) 
 */
 class Infected {
-  constructor (next, prev) {
+  constructor (next, prev, stochastic=false) {
     this.equation = "I"
     
     for (var x in next) {
-      this.equation += "-(I*" + String(next[x]) + ")"
+      if (stochastic == true) {
+        this.equation += "-((I*" + String(next[x]) + ")+sqrt(I*"+String(next[x])+"))"
+      } else {
+        this.equation += "-(I*" + String(next[x]) + ")"
+      }
     }
 
     for (var y in prev) {
-      this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      if (stochastic == true) {
+        this.equation += "+(("+String(prev[y][0])+"*" + String(prev[y][1])+")+sqrt("+String(prev[y][0])+"*" + String(prev[y][1])+"))*w"
+      } else {
+        this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      }
     }
   }
 
@@ -143,6 +172,10 @@ class Infected {
     for (y in parsed) {
       if (parsed[y] in key) {
         parsed.splice(y, 1, key[parsed[y]])
+      }
+      else if (parsed[y] === "w") {
+        distribution = new gaussian(0, 1)
+        parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
 
@@ -156,6 +189,7 @@ class Infected {
  * @param {Array} prev List of rates of the previous compartments, which include sub-arrays
  *       with the compartment id (one letter only), as a string, and the rate for the compartment.
  *       If reffering to this compartment's population, use "E" as the id.
+ * @param {Boolean} stochastic If true, the compartment will be stochastic. Defaults to false, optional.
  * @example
  * 
  *      // Note that you can pass in a string as a rate too, 
@@ -164,15 +198,23 @@ class Infected {
  *      let E = new Exposed([1/14], ["S*0.4/N"]) 
 */
 class Exposed {
-  constructor (next, prev) {
+  constructor (next, prev, stochastic=false) {
     this.equation = "E"
     
     for (var x in next) {
-      this.equation += "-(E*" + String(next[x]) + ")"
+      if (stochastic == true) {
+        this.equation += "-((E*" + String(next[x]) + ")+sqrt(E*"+String(next[x])+"))"
+      } else {
+        this.equation += "-(E*" + String(next[x]) + ")"
+      }
     }
 
     for (var y in prev) {
-      this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      if (stochastic == true) {
+        this.equation += "+(("+String(prev[y][0])+"*" + String(prev[y][1])+")+sqrt("+String(prev[y][0])+"*" + String(prev[y][1])+"))*w"
+      } else {
+        this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      }
     }
   }
 
@@ -191,6 +233,10 @@ class Exposed {
     for (y in parsed) {
       if (parsed[y] in key) {
         parsed.splice(y, 1, key[parsed[y]])
+      }
+      else if (parsed[y] === "w") {
+        distribution = new gaussian(0, 1)
+        parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
 
@@ -204,6 +250,7 @@ class Exposed {
  * @param {Array} prev List of rates of the previous compartments, which include sub-arrays
  *       with the compartment id (one letter only), as a string, and the rate for the compartment.
  *       If reffering to this compartment's population, use "C" as the id.
+ * @param {Boolean} stochastic If true, the compartment will be stochastic. Defaults to false, optional.
  * @example
  * 
  *      // Note that you can pass in a string as a rate too, 
@@ -212,15 +259,23 @@ class Exposed {
  *      let C = new Critical([0.14, 0.1], [["H", 0.3]]) 
 */
 class Critical {
-  constructor (next, prev) {
+  constructor (next, prev, stochastic=false) {
     this.equation = "C"
     
     for (var x in next) {
-      this.equation += "-(C*" + String(next[x]) + ")"
+      if (stochastic == true) {
+        this.equation += "-((C*" + String(next[x]) + ")+sqrt(C*"+String(next[x])+"))"
+      } else {
+        this.equation += "-(C*" + String(next[x]) + ")"
+      }
     }
 
     for (var y in prev) {
-      this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      if (stochastic == true) {
+        this.equation += "+(("+String(prev[y][0])+"*" + String(prev[y][1])+")+sqrt("+String(prev[y][0])+"*" + String(prev[y][1])+"))*w"
+      } else {
+        this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      }
     }
   }
 
@@ -239,6 +294,10 @@ class Critical {
     for (y in parsed) {
       if (parsed[y] in key) {
         parsed.splice(y, 1, key[parsed[y]])
+      }
+      else if (parsed[y] === "w") {
+        distribution = new gaussian(0, 1)
+        parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
 
@@ -252,6 +311,7 @@ class Critical {
  * @param {Array} prev List of rates of the previous compartments, which include sub-arrays
  *       with the compartment id (one letter only), as a string, and the rate for the compartment.
  *       If reffering to this compartment's population, use "H" as the id.
+ * @param {Boolean} stochastic If true, the compartment will be stochastic. Defaults to false, optional.
  * @example
  * 
  *      // Note that you can pass in a string as a rate too, 
@@ -260,15 +320,23 @@ class Critical {
  *      let H = new Hospitalized([0.3], [["I", 0.1], ["E", 0.2]])
 */
 class Hospitalized {
-  constructor (next, prev) {
+  constructor (next, prev, stochastic=false) {
     this.equation = "H"
     
     for (var x in next) {
-      this.equation += "-(H*" + String(next[x]) + ")"
+      if (stochastic == true) {
+        this.equation += "-((H*" + String(next[x]) + ")+sqrt(H*"+String(next[x])+"))"
+      } else {
+        this.equation += "-(H*" + String(next[x]) + ")"
+      }
     }
 
     for (var y in prev) {
-      this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      if (stochastic == true) {
+        this.equation += "+(("+String(prev[y][0])+"*" + String(prev[y][1])+")+sqrt("+String(prev[y][0])+"*" + String(prev[y][1])+"))*w"
+      } else {
+        this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      }
     }
   }
 
@@ -287,6 +355,10 @@ class Hospitalized {
     for (y in parsed) {
       if (parsed[y] in key) {
         parsed.splice(y, 1, key[parsed[y]])
+      }
+      else if (parsed[y] === "w") {
+        distribution = new gaussian(0, 1)
+        parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
 
@@ -301,6 +373,7 @@ class Hospitalized {
  * @param {Array} prev List of rates of the previous compartments, which include sub-arrays
  *       with the compartment id (one letter only), as a string, and the rate for the compartment.
  *       If reffering to this compartment's population, use "D" as the id.
+ * @param {Boolean} stochastic If true, the compartment will be stochastic. Defaults to false, optional.
  * @example
  * 
  *      // Note that you can pass in a string as a rate too, 
@@ -309,15 +382,23 @@ class Hospitalized {
  *      let D = new Dead([0.3], [["I", 0.3]]) // This disease also gives you a 3/10 chance to come alive after death.
 */
 class Dead {
-  constructor (next, prev) {
+  constructor (next, prev, stochastic=false) {
     this.equation = "D"
     
     for (var x in next) {
-      this.equation += "-(D*" + String(next[x]) + ")"
+      if (stochastic == true) {
+        this.equation += "-((D*" + String(next[x]) + ")+sqrt(D*"+String(next[x])+"))"
+      } else {
+        this.equation += "-(D*" + String(next[x]) + ")"
+      }
     }
 
     for (var y in prev) {
-      this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      if (stochastic == true) {
+        this.equation += "+(("+String(prev[y][0])+"*" + String(prev[y][1])+")+sqrt("+String(prev[y][0])+"*" + String(prev[y][1])+"))*w"
+      } else {
+        this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      }
     }
   }
 
@@ -336,6 +417,10 @@ class Dead {
     for (y in parsed) {
       if (parsed[y] in key) {
         parsed.splice(y, 1, key[parsed[y]])
+      }
+      else if (parsed[y] === "w") {
+        distribution = new gaussian(0, 1)
+        parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
 
@@ -349,6 +434,7 @@ class Dead {
  * @param {Array} prev List of rates of the previous compartments, which include sub-arrays
  *       with the compartment id (one letter only), as a string, and the rate for the compartment.
  *       If reffering to this compartment's population, use "V" as the id.
+ * @param {Boolean} stochastic If true, the compartment will be stochastic. Defaults to false, optional.
  * @example
  * 
  *      // Note that you can pass in a string as a rate too, 
@@ -357,15 +443,23 @@ class Dead {
  *      let I = new Infected([0.001], ["S*0.4"])
 */
 class Vaccinated {
-  constructor (next, prev) {
+  constructor (next, prev, stochastic=false) {
     this.equation = "V"
     
     for (var x in next) {
-      this.equation += "-(V*" + String(next[x]) + ")"
+      if (stochastic == true) {
+        this.equation += "-((V*" + String(next[x]) + ")+sqrt(V*"+String(next[x])+"))"
+      } else {
+        this.equation += "-(V*" + String(next[x]) + ")"
+      }
     }
 
     for (var y in prev) {
-      this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      if (stochastic == true) {
+        this.equation += "+(("+String(prev[y][0])+"*" + String(prev[y][1])+")+sqrt("+String(prev[y][0])+"*" + String(prev[y][1])+"))*w"
+      } else {
+        this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      }
     }
   }
 
@@ -384,6 +478,10 @@ class Vaccinated {
     for (y in parsed) {
       if (parsed[y] in key) {
         parsed.splice(y, 1, key[parsed[y]])
+      }
+      else if (parsed[y] === "w") {
+        distribution = new gaussian(0, 1)
+        parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
 
@@ -397,6 +495,7 @@ class Vaccinated {
  * @param {Array} prev List of rates of the previous compartments, which include sub-arrays
  *       with the compartment id (one letter only), as a string, and the rate for the compartment.
  *       If reffering to this compartment's population, use "V" as the id.
+ * @param {Boolean} stochastic If true, the compartment will be stochastic. Defaults to false, optional.
  * @example
  * 
  *      // Note that you can pass in a string as a rate too, 
@@ -405,15 +504,23 @@ class Vaccinated {
  *      let R = new Recovered([ ], [["I", 0.1]])
 */
 class Recovered {
-  constructor (next, prev) {
+  constructor (next, prev, stochastic=false) {
     this.equation = "R"
     
     for (var x in next) {
-      this.equation += "-(R*" + String(next[x]) + ")"
+      if (stochastic == true) {
+        this.equation += "-((R*" + String(next[x]) + ")+sqrt(R*"+String(next[x])+"))"
+      } else {
+        this.equation += "-(R*" + String(next[x]) + ")"
+      }
     }
 
     for (var y in prev) {
-      this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      if (stochastic == true) {
+        this.equation += "+(("+String(prev[y][0])+"*" + String(prev[y][1])+")+sqrt("+String(prev[y][0])+"*" + String(prev[y][1])+"))*w"
+      } else {
+        this.equation += "+("+String(prev[y][0])+"*" + String(prev[y][1])+")"
+      }
     }
   }
 
@@ -432,6 +539,10 @@ class Recovered {
     for (y in parsed) {
       if (parsed[y] in key) {
         parsed.splice(y, 1, key[parsed[y]])
+      }
+      else if (parsed[y] === "w") {
+        distribution = new gaussian(0, 1)
+        parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
 

tests/scripts/comp.js
@@ -2,6 +2,6 @@ let susceptible2 = new Idiom("S-(B*S*I/p)");
 let infected2 = new Idiom("I+(B*S*I/p)-(u*I)");
 let recovered2 = new Idiom("R+(u*I)");
 
-let S = new Susceptible(["I*0.4/N"], []);
-let I = new Infected([0.2], [['S', 'I*0.4/N']]);
-let R = new Recovered([], [['I', 0.2]]);
+let S = new Susceptible(["I*0.4/N"], [], true);
+let I = new Infected([0.2], [['S', 'I*0.4/N']], true);
+let R = new Recovered([], [['I', 0.2]], true);

commit 30a7661027a0c070c51358c08348fb25ba8f3400

Fix comp.js bug introduced in c79c74e
Fix comp.js bug introduced in c79c74e which defined an undeclared variable without prefixing it with "let", "var", or "const".

Signed-off-by: Quantalabs <55121845+Quantalabs@users.noreply.github.com>

EpiJS/comp.js
@@ -50,7 +50,7 @@ class Idiom {
         parsed.splice(y, 1, key[parsed[y]])
       }
       else if (parsed[y] === "w") {
-        distribution = new gaussian(0, 1)
+        var distribution = new gaussian(0, 1)
         parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
@@ -113,7 +113,7 @@ class Susceptible {
         parsed.splice(y, 1, key[parsed[y]])
       }
       else if (parsed[y] === "w") {
-        distribution = new gaussian(0, 1)
+        var distribution = new gaussian(0, 1)
         parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
@@ -174,7 +174,7 @@ class Infected {
         parsed.splice(y, 1, key[parsed[y]])
       }
       else if (parsed[y] === "w") {
-        distribution = new gaussian(0, 1)
+        var distribution = new gaussian(0, 1)
         parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
@@ -235,7 +235,7 @@ class Exposed {
         parsed.splice(y, 1, key[parsed[y]])
       }
       else if (parsed[y] === "w") {
-        distribution = new gaussian(0, 1)
+        var distribution = new gaussian(0, 1)
         parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
@@ -296,7 +296,7 @@ class Critical {
         parsed.splice(y, 1, key[parsed[y]])
       }
       else if (parsed[y] === "w") {
-        distribution = new gaussian(0, 1)
+        var distribution = new gaussian(0, 1)
         parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
@@ -357,7 +357,7 @@ class Hospitalized {
         parsed.splice(y, 1, key[parsed[y]])
       }
       else if (parsed[y] === "w") {
-        distribution = new gaussian(0, 1)
+        var distribution = new gaussian(0, 1)
         parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
@@ -419,7 +419,7 @@ class Dead {
         parsed.splice(y, 1, key[parsed[y]])
       }
       else if (parsed[y] === "w") {
-        distribution = new gaussian(0, 1)
+        var distribution = new gaussian(0, 1)
         parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
@@ -480,7 +480,7 @@ class Vaccinated {
         parsed.splice(y, 1, key[parsed[y]])
       }
       else if (parsed[y] === "w") {
-        distribution = new gaussian(0, 1)
+        var distribution = new gaussian(0, 1)
         parsed.splice(y, 1, distribution.random(1)[0])
       }
     }
@@ -541,7 +541,7 @@ class Recovered {
         parsed.splice(y, 1, key[parsed[y]])
       }
       else if (parsed[y] === "w") {
-        distribution = new gaussian(0, 1)
+        var distribution = new gaussian(0, 1)
         parsed.splice(y, 1, distribution.random(1)[0])
       }
     }