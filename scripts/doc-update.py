2fa8e8092c8e8a8343b052e14e6a491e6cbfbec3
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-07-21 17:53:54 -0700

    Fix error in documentation

--- scripts/doc-update.py
+++ scripts/doc-update.py
@@ -27,26 +27,22 @@ with open('../docs/com.md', 'r') as comdoc:
 with open('../docs/com.md', 'w') as comdoc:
   comdoc.writelines(doc)
 
+
 with open('../docs/model.md', 'r') as mdoc:
+
   doc = mdoc.readlines()
-  doc[16] = '# Model\n'
-  doc[21] = '```javascript\n'
-  doc[23] = '```\n'
-  doc[115] = "[1]: #model-2\n"
-  doc[119] = '[3]: #get-data\n'
+  doc[4] = '\n'
+  doc[12] = '# Model\n'
+  doc[17] = '```javascript\n'
+  doc[19] = '```\n'
+  doc[77] = "[1]: #model-2\n"
+  doc[81] = '[3]: #get-data\n'
   doc1 = ['---\n', 'title: EpiJS Module - Model\n', '---\n']
-  
-  for x in range(24, 58):
-    doc[x] = ''
-  
-  for x in range(7, 14):
-    doc[x] = ''
-
   doc = doc1+doc
-
 
 with open('../docs/model.md', 'w') as mdoc:
   mdoc.writelines(doc)
+
 
 with open('../docs/comp.md', 'r') as compdoc:
   doc = compdoc.readlines()
