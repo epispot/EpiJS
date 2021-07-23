1fb4cab279357b9496ddc1a9961b996207ab7a88
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-07-23 14:17:55 -0700

    Update doc scripts

--- scripts/doc-build.sh
+++ scripts/doc-build.sh
@@ -1,7 +1,7 @@
 documentation build EpiJS/pre.js -f md > docs/pre.md 
 documentation build EpiJS/comp.js -f md > docs/comp.md 
 documentation build EpiJS/com.js -f md > docs/com.md
-documentation build EpiJS/model.js -f md > docs/model.md
+documentation build EpiJS/model.js -f md > docs/model.md --shallow
 documentation build EpiJS/utils.js -f md > docs/utils.md
 documentation build EpiJS/plots.js -f md > docs/plots.md
 cd scripts 
