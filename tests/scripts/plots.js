ba5c64510470a5f64cef2d9d55f5ff9ffa6c51a9
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-07-23 16:03:25 -0700

    `v2.1.0`

--- tests/scripts/plots.js
+++ tests/scripts/plots.js
@@ -1,6 +1,7 @@
-
-plot(sirout1, "canvas-pre1", 100)
+let newplot = plot(sirout1, "canvas-pre1", 100)
 plot(seirout1, "canvas-pre2", 100)
 plot(seirdout1, "canvas-pre3", 100)
 plot(seihrdout1, "canvas-pre4", 100)
 plot(seihcrdout1, "canvas-pre5", 100)
+
+manipulate(newplot, "fill", true)
