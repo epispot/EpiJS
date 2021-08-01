epispot/EpiJS

commit c30c8d3aa18d4acb35f96b8f4ddc0f0c6e624758

Bugfix (#96)
* Bug fix (see 3349670)

* Rebuild code from commit c092bb3

EpiJS/model.js
@@ -63,6 +63,15 @@ class Model {
 	 *model.get_data(100) // Get data for 100 days.
 	 */
 	get_data(time) { // skipcq: JS-0045
+		var key = {}
+		var newkey = {}
+		
+		// For key in this.key, add it to newkey and key.
+		for (var key_key in this.key) {
+			key[key_key] = this.key[key_key]
+			newkey[key_key] = this.key[key_key]
+		}
+
 		var key = this.key
 		var newkey = key
 

commit a311ff8c2c9775e84e2b4c3e07db909f5c59c6fc

Fix bug from c30c8d3
Fixes a bug introduced in c30c8d3

EpiJS/model.js
@@ -72,9 +72,6 @@ class Model {
 			newkey[key_key] = this.key[key_key]
 		}
 
-		var key = this.key
-		var newkey = key
-
 		for (var x = 0; x < time; x++) {
 			var y; // skipcq: JS-0119
 			for (y in this.compartments) {