fd673b5e289ba4581f9d882fa342e544a8a283f7
QLabs <55121845+Quantalabs@users.noreply.github.com>
2022-04-05 11:03:14 -0700

    Documentation Enhancements (#142)
    - `.github/workfows/docs.yml`
      - Fix documentation workflow
    - `EpiJS/comp.js`
      - Edit jsdoc comments for better rendering in final documentation output.
    - `EpiJS/index.js`
      - Change `package` variable to `packagejson` for JSDoc parsing errors.
    - `docs/.vuepress/config.js`
      - Update configuration to add plugins and themes
    - `docs/.vuepress/dist/**`
      - Rebuild documentation
    - `docs/.vuepress/styles/index.styl`
      - Edit styling for `tabs` plugin
    - `docs/.vuepress/styles/pallette.styl`
      - Edit progress bar color
    - `docs/README.md`
      - Update landing page
    - `docs/*.md`
      - Rebuild docs using `jsdoc-to-markdown`
    - `package.json`
      - Update documentation scripts
      - Add vuepress plugins and other packages
    - `scripts/*`
      - Remove previous documentation scripts
      - Add new documentation script with `jsdoc-to-markdown`

--- scripts/doc-build.sh
+++
@@ -1,9 +0,0 @@
-documentation build EpiJS/pre.js -f md > docs/pre.md --shallow
-documentation build EpiJS/comp.js -f md > docs/comp.md --shallow
-documentation build EpiJS/model.js -f md > docs/model.md --shallow
-documentation build EpiJS/utils.js -f md > docs/utils.md --shallow
-documentation build EpiJS/plots.js -f md > docs/plots.md --shallow
-cd scripts 
-python doc-update.py
-cd .. 
-vuepress build docs
